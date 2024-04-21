7f7a850e5e6ac8463d0cda06c0d1a6ff004c37c3
Ali ben <alibenhenia1@gmail.com>
2024-04-21 21:51:21 +0100

    add

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -13,8 +13,7 @@ const About = ()=>{
                      <span className="line_"></span>
                  </div>
                     <div className="p-5">
-                            // write short about yourself and style it tailwindcss
-                            <p className="text-lg">I am a full stack developer with 3 years of experience in web development. I have worked with various technologies like React, Nodejs, Express, MongoDB, and many more. I have a good understanding of the MERN stack and I am always eager to learn new technologies. I have worked on various projects and I have a good understanding of the software development life cycle. I am a quick learner and I am always ready to take on new challenges.</p>
+                    <p className="text-lg">I am a full stack developer with 3 years of experience in web development</p>
                     </div>
             </div>
         </div>
